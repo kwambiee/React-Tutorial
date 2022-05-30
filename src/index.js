dbce6de7d68454d72d37abd662f91757257ef2c7
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-30 11:00:52 +0300

    Add state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>To-Do react app</h1>
-ReactDOM.render(element,document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
